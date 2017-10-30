1d5c1e1784e638562c048b3c9fa473d1a3f344bf
K V K VENUGOPAL <kvkvenugopal2013@gmail.com>
2017-10-30 15:47:24 +0530

    Update and rename jarvis.py to ET_THE_UAI.py

---
+++ ET_THE_UAI.py
@@ -0,0 +1,1 @@
+""" This is ET, THE UNREALISTIC AI. We have got a lot things coming up. Hopefully, the Python code goes below. Lots and lots of stuff coming up."""
--- jarvis.py
+++
@@ -1,2 +0,0 @@
-""" This is JARVIS, my personal Assistant. We have got a lot for future.
-So let's begin here."""
