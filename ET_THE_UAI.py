e6a3737ea2d51a59d5c861883e74e83725934660
K V K VENUGOPAL <kvkvenugopal2013@gmail.com>
2017-10-30 15:47:46 +0530

    Update ET_THE_UAI.py

--- ET_THE_UAI.py
+++ ET_THE_UAI.py
@@ -1,1 +1,3 @@
-""" This is ET, THE UNREALISTIC AI. We have got a lot things coming up. Hopefully, the Python code goes below. Lots and lots of stuff coming up."""
+""" This is ET, THE UNREALISTIC AI. We have got a lot things coming up. 
+Hopefully, the Python code goes below. 
+Lots and lots of stuff coming up."""
